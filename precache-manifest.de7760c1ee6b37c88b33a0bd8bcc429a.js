9c65a4dd0bf22dc7fa165798bbd3516234436e7d
Louis Hellman <louis.hellman@gmail.com>
2020-07-30 16:06:38 -0700

    Updates

---
+++ precache-manifest.de7760c1ee6b37c88b33a0bd8bcc429a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d82e5b58da2e596a9a7848dff1367314",
+    "url": "/starwars-horoscope/index.html"
+  },
+  {
+    "revision": "410bb053ab753f5c95a3",
+    "url": "/starwars-horoscope/static/css/2.5f0f5072.chunk.css"
+  },
+  {
+    "revision": "9aafd61678b769d3ee4c",
+    "url": "/starwars-horoscope/static/css/main.4bd99868.chunk.css"
+  },
+  {
+    "revision": "410bb053ab753f5c95a3",
+    "url": "/starwars-horoscope/static/js/2.94229a49.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/starwars-horoscope/static/js/2.94229a49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aafd61678b769d3ee4c",
+    "url": "/starwars-horoscope/static/js/main.0fa0b5fa.chunk.js"
+  },
+  {
+    "revision": "f70efa50dab8e1679c92",
+    "url": "/starwars-horoscope/static/js/runtime-main.8d99a908.js"
+  },
+  {
+    "revision": "35f31c7aa7cc82eaec5d5c37ef2de044",
+    "url": "/starwars-horoscope/static/media/STARWARS.35f31c7a.woff"
+  }
+]);
